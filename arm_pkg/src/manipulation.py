d2461d299e81acf7764044696bd0de12a8c34db0
happy-isiyama <b1930722@planet.kanazawa-it.ac.jp>
2023-03-13 18:04:56 +0900

    instance desk

--- arm_pkg/src/manipulation.py
+++ arm_pkg/src/manipulation.py
@@ -56,7 +56,33 @@ class Arm():
         tts.say('把持に成功しました')
         gripper.command(1.0)
         sys.exit()
+        
+    def instance_desk(self,object_name):
+        topic_tf = object_name
+        object_to_hand = geometry.pose(z=0, ek=-1.57)
+        
+        # handを0.1[m]上に移動させる姿勢
+        hand_up = geometry.pose(x=0.1)
+        # handを0.5[m]手前に移動させる姿勢
+        hand_back = geometry.pose(z=-0.5)
 
+        gripper.command(1.0)
+        whole_body.move_to_neutral()
+        whole_body.looking_hand_constraint = True
+        whole_body.move_end_effector_pose(object_to_hand, topic_tf)
+        # 力を指定して把持する
+        gripper.apply_force(_GRASP_FORCE)
+        # シミュレータのgrasp hackのための待ち時間。実機では不要
+        rospy.sleep(2.0)
+        # 手先相対で上にハンドを移動
+        whole_body.move_end_effector_pose(hand_up, _HAND_TF)
+        # 手先相対で後ろにハンドを移動
+        whole_body.move_end_effector_pose(hand_back, _HAND_TF)
+        # 初期姿勢に遷移
+        whole_body.move_to_neutral()
+        tts.say('把持に成功しました')
+        gripper.command(1.0)
+        sys.exit()
 
     def ar_floor(self,ar):
         topic_tf = 'ar_marker/' + ar
@@ -96,4 +122,4 @@ class Arm():
 
 if __name__=='__main__':
     arm = Arm()
-    arm.ar_floor('201')
+    arm.instance_floor("bottle")
